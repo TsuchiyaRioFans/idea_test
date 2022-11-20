1e44bbb4ff987a70b0e1cb5015c5cd3908f21878
zhz <zhz15114585761@hrbeu.edu.cn>
2022-11-20 11:22:02 +0800

    2022-11-20

--- src/Main.java
+++ src/Main.java
@@ -16,80 +16,41 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-
-    class Pair{
-        String in;
-        String out;
-
-        public Pair(String in, String out) {
-            this.in = in;
-            this.out = out;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Pair pair = (Pair) o;
-            return in.equals(pair.in) && out.equals(pair.out);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(in, out);
-        }
-    }
-
-
-    class UndergroundSystem {
-
-        Map<Integer,Integer> map;
-        Map<Pair,List<Integer>> map1;
-        Map<Integer,String> map2;
-        public UndergroundSystem() {
-            map = new HashMap<>();
-            map1 = new HashMap<>();
-            map2 = new HashMap<>();
-        }
-
-        public void checkIn(int id, String stationName, int t) {
-            map.put(id,t);
-            map2.put(id,stationName);
-        }
-
-        public void checkOut(int id, String stationName, int t) {
-            int startTime = map.get(id);
-            String startStation = map2.get(id);
-            int time = t-startTime;
-            Pair pair = new Pair(startStation,stationName);
-            if(map1.containsKey(pair)){
-                map1.get(pair).add(time);
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        if(poured==0)
+            return 0D;
+        double[][] count = new double[query_row+1][query_row+1];
+        count[0][0]=poured;
+        int pos = 0;
+        while (pos<=query_row){
+            boolean flag = true;
+            for(int i=0;i<=query_row;i++){
+                if(count[pos][i]>1){
+                    if(pos+1<=query_row){
+                        double more = (count[pos][i]-1.0)/2.0;
+                        count[pos][i]=1.0D;
+                        count[pos+1][i]+=more;
+                        count[pos+1][i+1]+=more;
+                        flag = false;
+                    }
+                    else{
+                        count[pos][i] = 1.0D;
+                    }
+                }
             }
-            else{
-                List<Integer> list = new ArrayList<>();
-                list.add(time);
-                map1.put(pair,list);
-            }
+            if(flag)
+                break;
+            pos++;
         }
-
-        public double getAverageTime(String startStation, String endStation) {
-            Pair pair = new Pair(startStation,endStation);
-            if(!map1.containsKey(pair))
-                return 0D;
-            List<Integer> list = map1.get(pair);
-            double ans = 0;
-            for(int i=0;i<list.size();i++){
-                ans+=list.get(i);
-            }
-            return ans/list.size();
-        }
+        return count[query_row][query_glass];
     }
 
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{0,1,-2,-3,4};
+        int[] nums = new int[]{3,0,2,5,4};
         int[][] nums1 = new int[][]{{1,5},{1,1},{1,6},{0,2}};
-        System.out.println(1);
+        main.champagneTower(100000009,33,17);
+
     }
 }
