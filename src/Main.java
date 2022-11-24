652806ccb1e14d55cf320fc8ac2b900dfaed206f
zhz <zhz15114585761@hrbeu.edu.cn>
2022-11-24 09:44:31 +0800

    2022-11-24

--- src/Main.java
+++ src/Main.java
@@ -16,60 +16,57 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    static class SnapshotArray {
-        int count;
-        Map<Integer,Map<Integer,Integer>> map;
-
-        int[] nums;
-        List<Integer> change;
-        public SnapshotArray(int length) {
-            map = new HashMap<>();
-            for(int i=0;i<length;i++){
-                map.put(i,new HashMap<>());
+    boolean flag;
+    public void dfs(List<Integer> list,String s,int pos){
+        if(flag)
+            return;
+        if(pos==s.length()){
+            if(list.size()>2)
+                flag = true;
+            return;
+        }
+        for(int i=pos+1;i<=s.length();i++){
+            String temp = s.substring(pos,i);
+            long val1 = Long.parseLong(temp);
+            if(val1>Integer.MAX_VALUE)
+                break;
+            int val = Integer.parseInt(temp);
+            if(temp.length()>=2&&temp.charAt(0)=='0')
+                break;
+            if(list.size()<=1){
+                list.add(val);
+                dfs(list,s,i);
             }
-            this.count = 0;
-            nums = new int[length];
-            change = new ArrayList<>();
-        }
-
-        public void set(int index, int val) {
-            nums[index] = val;
-            change.add(index);
-        }
-
-        public int snap() {
-            for(int i=0;i<change.size();i++){
-                int pos = change.get(i);
-                map.get(pos).put(count,nums[pos]);
-            }
-            count++;
-            change.clear();
-            return count-1;
-        }
-
-        public int get(int index, int snap_id) {
-            Map<Integer,Integer> tempMap = map.get(index);
-            int temp = snap_id;
-            while (temp>=0){
-                if(tempMap.containsKey(temp)){
-                    return tempMap.get(temp);
+            else{
+                int sum = list.get(list.size()-1)+list.get(list.size()-2);
+                if(sum>val){
+                    continue;
                 }
-                temp--;
+                else if(sum==val){
+                    list.add(sum);
+                    dfs(list,s,i);
+                }
+
+                else
+                    break;
             }
-            return 0;
+            if(flag)
+                return;
+            list.remove(list.size()-1);
         }
     }
+    public List<Integer> splitIntoFibonacci(String num) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(ans,num,0);
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
         int[] nums = new int[]{4,2,4,5,6};
         int[][] nums1 = new int[][]{{1,5},{1,1},{1,6},{0,2}};
-        SnapshotArray snapshotArray = new SnapshotArray(3);
-        snapshotArray.set(0,4);
-        snapshotArray.set(0,16);
-        snapshotArray.set(0,13);
-        snapshotArray.snap();
-        snapshotArray.get(0,0);
-        snapshotArray.snap();
+        main.splitIntoFibonacci("5511816597");
     }
 }
