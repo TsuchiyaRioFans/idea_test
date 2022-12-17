61f68b0cef815e206be87cb60f42aac77d9f58cd
zhz <zhz15114585761@hrbeu.edu.cn>
2022-12-17 10:11:54 +0800

    2022-12.17

--- src/Main.java
+++ src/Main.java
@@ -19,32 +19,47 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    public int minElements(int[] nums, int limit, int goal) {
-        BigInteger sum = new BigInteger("0");
-        for(int n:nums){
-            sum = sum.add(new BigInteger(String.valueOf(n)));
+    public boolean canChoose(int[][] groups, int[] nums) {
+        int pos = 0;
+        for(int[] group:groups){
+            boolean flag1 = false;
+            while (pos<nums.length){
+                if(nums[pos]==group[0]){
+                    boolean flag = true;
+                    int temp = pos+1;
+                    int pos1 = 1;
+                    while (temp<nums.length&&pos1<group.length){
+                        if(nums[temp]!=group[pos1]){
+                            flag = false;
+                            break;
+                        }
+                        temp++;
+                        pos1++;
+                    }
+                    if(flag&&pos1==group.length){
+                        flag1 = true;
+                        pos = temp;
+                        break;
+                    }
+                    else{
+                        pos++;
+                    }
+                }
+                else{
+                    pos++;
+                }
+            }
+            if(!flag1)
+                return false;
         }
-        BigInteger goal1 = new BigInteger(String.valueOf(goal));
-        sum = goal1.subtract(sum);
-        if(sum.signum()<0)
-            sum = sum.multiply(new BigInteger("-1"));
-        if(sum.signum()==0)
-            return 0;
-        int ans = 0;
-        BigInteger temp = new BigInteger("0");
-        BigInteger max = new BigInteger(String.valueOf(limit));
-        temp = sum.divide(max);
-        if(sum.mod(max).compareTo(new BigInteger("0"))>0){
-            temp = temp.add(new BigInteger("1"));
-        }
-        return Integer.parseInt(String.valueOf(temp));
+        return true;
     }
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{43024,99908};
+        int[] nums = new int[]{1,-1,0,1,-1,-1,3,-2,0};
         String[] ss = new String[]{"gta","gta(1)","gta","avalon"};
-        int[][] nums1 = new int[][]{{1,2,4},{3,3,1}};
-        System.out.println(Math.pow(10,14)>Long.MAX_VALUE);
+        int[][] nums1 = new int[][]{{-5,0}};
+        main.canChoose(nums1,nums);
     }
 }
