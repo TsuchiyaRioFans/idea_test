7911d6ef8d99c7a2362a84bc5609b6ed52ed7020
zhz <zhz15114585761@hrbeu.edu.cn>
2023-01-06 11:43:13 +0800

    2023-01-06

--- src/Main.java
+++ src/Main.java
@@ -26,50 +26,72 @@ public class Main {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
 
-
-    public int maxValue(int n, int index, int maxSum) {
-        int left = 1;
-        int right = maxSum;
-        while (left<right){
-            int mid = left+(right-left)/2;
-            if(isValid(index,mid,maxSum,n)){
-                left = mid+1;
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        int n = barcodes.length;
+        int[] ans = new int[n];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int m:barcodes){
+            map.put(m,1+map.getOrDefault(m,0));
+        }
+        Queue<Map.Entry<Integer,Integer>> queue = new
+                PriorityQueue<>((a,b)->b.getValue().compareTo(a.getValue()));
+        for(Map.Entry<Integer,Integer> entry:map.entrySet())
+            queue.offer(entry);
+        int pos = 0;
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>();
+        while (!queue.isEmpty()){
+            Map.Entry<Integer,Integer> entry = queue.poll();
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if(pos==0||ans[pos-1]!=key){
+                ans[pos] = key;
+                if(value>1){
+                    entry.setValue(value-1);
+                    queue.offer(entry);
+                }
             }
             else{
-                right = mid-1;
+                list.add(entry);
+                while (!queue.isEmpty()){
+                    Map.Entry<Integer,Integer> temp = queue.poll();
+                    if(temp.getKey()!=ans[pos-1]){
+                        ans[pos] = temp.getKey();
+                        if(temp.getValue()>1){
+                            temp.setValue(temp.getValue()-1);
+                            list.add(temp);
+                        }
+                        break;
+                    }
+                    list.add(temp);
+                }
+                for(int i=0;i<list.size();i++)
+                    queue.offer(list.get(i));
+                list.clear();
             }
+            pos++;
         }
-        if(isValid(index,left,maxSum,n))
-            return left;
-        return left-1;
+        return ans;
     }
 
-    public boolean isValid(int index,int val,int max,int n){
-        long count = 0;
-        if(val>index){
-            count+= (long) (val + val - index) *(1+index)/2;
-        }
-        else{
-            count+=((index-val+1)+ (long) (1 + val) *(val)/2);
-        }
-        if(count>max)
-            return false;
-        if(val>=(n-index)){
-            count+= (long) (val + val - (n - index)+1) *(n-index)/2;
-        }
-        else{
-            count+=(n-index-val)+ (long) (1 + val) *(val)/2;
-        }
-        count-=val;
-        return count<=max;
-    }
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{1,0,0,-1};
+        int[] nums = new int[]{2,2,1,3};
         String[] ss = new String[]{"a","b","leetcode"};
         int[][] nums1 = new int[][]{{-5,0}};
-        main.maxValue(4,2,6);
+        main.rearrangeBarcodes(nums);
     }
 }
