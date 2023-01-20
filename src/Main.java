1a7b4e923018484d898627ecef80597866d5f509
zhz <zhz15114585761@hrbeu.edu.cn>
2023-01-20 09:28:12 +0800

    2023-01-20

--- src/Main.java
+++ src/Main.java
@@ -39,60 +39,68 @@ public class Main {
       }
     }
 
+    public List<String> alertNames(String[] keyName, String[] keyTime) {
+        Set<String> set = new HashSet<>();
+        Map<String,List<String>> map = new HashMap<>();
+        for(int i=0;i<keyName.length;i++){
+            if(set.contains(keyName[i]))
+                continue;
+            if(map.containsKey(keyName[i]))
+                map.get(keyName[i]).add(keyTime[i]);
+            else{
+                List<String> list = new ArrayList<>();
+                list.add(keyTime[i]);
+                map.put(keyName[i],list);
+            }
+            Collections.sort(map.get(keyName[i]),(a,b)->{
+                String[] ss1 = a.split(":");
+                String[] ss2 = b.split(":");
+                int a1 = Integer.parseInt(ss1[0]);
+                int a2 = Integer.parseInt(ss1[1]);
+                int b1 = Integer.parseInt(ss2[0]);
+                int b2 = Integer.parseInt(ss2[1]);
+                if(a1<b1)
+                    return -1;
+                if(a1>b1)
+                    return 1;
+                if(a2<b2)
+                    return -1;
+                if(a2>b2)
+                    return 1;
+                return 0;
+            });
+            List<String> temp = map.get(keyName[i]);
+            for(int j=0;j<temp.size()-2;j++){
+                if(fun(temp.get(j),temp.get(j+2))){
+                    set.add(keyName[i]);
+                    break;
+                }
+            }
+        }
+        List<String> ans = new ArrayList<>(set);
+        Collections.sort(ans);
+        return ans;
+    }
 
-    public int reachableNodes(int n, int[][] edges, int[] restricted) {
-        Set<Integer> set = new HashSet<>();
-        set.add(0);
-        Map<Integer,List<Integer>> map = new HashMap<>();
-        Set<Integer> unUse = new HashSet<>();
-        for(int val:restricted)
-            unUse.add(val);
-        for(int[] nums:edges) {
-            int left = nums[0];
-            int right = nums[1];
-            if (!unUse.contains(left) && !unUse.contains(right)) {
-                if (map.containsKey(left))
-                    map.get(left).add(right);
-                else {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(right);
-                    map.put(left, temp);
-                }
-                if (map.containsKey(right))
-                    map.get(right).add(left);
-                else {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(left);
-                    map.put(right, temp);
-                }
-            }
-        }
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
-        while (!queue.isEmpty()){
-            int length = queue.size();
-            for(int i=0;i<length;i++){
-                int val = queue.poll();
-                if(map.containsKey(val)){
-                    List<Integer> temp = map.get(val);
-                    for(int j=0;j<temp.size();j++){
-                        int t = temp.get(j);
-                        if(set.contains(t))
-                            continue;
-                        set.add(t);
-                        queue.offer(t);
-                    }
-                }
-            }
-        }
-        return set.size();
+    public boolean fun(String a,String b){
+        String[] ss1 = a.split(":");
+        String[] ss2 = b.split(":");
+        int a1 = Integer.parseInt(ss1[0]);
+        int a2 = Integer.parseInt(ss1[1]);
+        int b1 = Integer.parseInt(ss2[0]);
+        int b2 = Integer.parseInt(ss2[1]);
+        int ans = 0;
+        ans+=(b1-a1)*60;
+        ans+=(b2-a2);
+        return ans<=60;
     }
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{4,5};
-        String[] ss = new String[]{"a","b","leetcode"};
+        int[] nums = new int[]{1,1,1,1,1};
+        String[] ss1 = new String[]{"daniel","daniel","daniel","luis","luis","luis","luis"};
+        String[] ss2 = new String[]{"10:00","10:40","11:00","09:00","11:00","13:00","15:00"};
         int[][] nums1 = new int[][]{{0,1},{1,2},{3,1},{4,0},{0,5},{5,6}};
-        main.reachableNodes(7,nums1,nums);
+        main.alertNames(ss1,ss2);
     }
 }
