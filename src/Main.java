a8ed7617694547d5f0a6a38e1e384ae391ec2a81
zhz <zhz15114585761@hrbeu.edu.cn>
2023-04-25 13:11:05 +0800

    2023.04.25

--- src/Main.java
+++ src/Main.java
@@ -43,44 +43,190 @@ public class Main {
       }
     }
 
-    public String minRemoveToMakeValid(String s) {
-        Stack<int[]> stack = new Stack<>();
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<s.length();i++){
+    public List<List<Integer>> findMatrix(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        boolean[] visited = new boolean[n];
+        Arrays.sort(nums);
+        int count = 0;
+        while (true){
+            Set<Integer> set = new HashSet<>();
+            for(int i=0;i<n;i++){
+                if(visited[i])
+                    continue;
+                if(set.contains(nums[i]))
+                    continue;
+                set.add(nums[i]);
+                visited[i] = true;
+            }
+            count+=set.size();
+            ans.add(new ArrayList<>(set));
+            if(count==n)
+                break;
+        }
+        return ans;
+    }
+
+    public long[] findPrefixScore(int[] nums) {
+        int n = nums.length;
+        long[] ans = new long[n];
+        int max = nums[0];
+        ans[0] = 2L *nums[0];
+        for(int i=1;i<n;i++){
+            max = Math.max(max,nums[i]);
+            ans[i] = nums[i]+max;
+        }
+        return ans;
+    }
+
+    public int lastStoneWeightII(int[] ss) {
+        int n = ss.length;
+        int sum = 0;
+        for (int i : ss) sum += i;
+        int t = sum / 2;
+        int[][] f = new int[n + 1][t + 1];
+        for (int i = 1; i <= n; i++) {
+            int x = ss[i - 1];
+            for (int j = 0; j <= t; j++) {
+                f[i][j] = f[i - 1][j];
+                if (j >= x)
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - x] + x);
+            }
+        }
+        return Math.abs(sum - f[n][t] - f[n][t]);
+    }
+
+
+    public String solveEquation(String s) {
+        int x = 0,num = 0;
+        int n = s.length();
+        for(int i=0,op=1;i<n;){
             char ch = s.charAt(i);
-            if(ch=='(')
-                stack.push(new int[]{0,i});
-            else if(ch==')'){
-                if(!stack.isEmpty()&&stack.peek()[0]==0){
-                    stack.pop();
+            if(ch=='+'){
+                op=1;i++;
+            }
+            else if(ch=='-'){
+                op=-1;i++;
+            }
+            else if(ch=='='){
+                op=1;x*=-1;num*=-1;i++;
+            }
+            else{
+                int j =i;
+                while (j<n){
+                    char ch1 = s.charAt(j);
+                    if(ch1=='+'||ch1=='-'||ch1=='=')
+                        break;
+                    j++;
+                }
+                if(s.charAt(j-1)=='x'){
+                    x+= op*(i<j-1?(Integer.parseInt(s.substring(i,j-1))):1);
                 }
                 else{
-                    stack.push(new int[]{1,i});
+                    num+=op*Integer.parseInt(s.substring(i,j));
+                }
+                i=j;
+            }
+        }
+        if(x==0)
+            return num==0?"Infinite solutions":"No solution";
+        return "x="+(num/-x);
+    }
+
+    public int sumOfMultiples(int n) {
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0){
+                ans+=i;
+            }
+        }
+        return ans;
+    }
+
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<k;i++)
+            list.add(nums[i]);
+        Collections.sort(list);
+        int left = 0;
+        int right = k;
+        for(int i=0;i<ans.length;i++){
+            int val = list.get(x-1);
+            if(val<0)
+                ans[i] = val;
+            if(right<n){
+                int pos1 = Collections.binarySearch(list,nums[left]);
+                list.remove(pos1);
+                int pos2 = Collections.binarySearch(list,nums[right]);
+                if(pos2<0)
+                    pos2 = -1*pos2 -1;
+                list.add(pos2,nums[right]);
+                left++;
+                right++;
+            }
+        }
+        return ans;
+    }
+
+    public static int gcd(int a, int b) {
+        return b != 0 ? gcd(b, a%b) : a;
+    }
+    public int minOperations(int[] nums) {
+        boolean flag = false;
+        int n = nums.length;
+        for(int i=0;i<n-1;i++){
+            int val1 = nums[i];
+            int val2 = nums[i+1];
+            if(val1%2==0&&val2%2==1)
+                flag = true;
+            if(val1%2==1&&val2%2==0)
+                flag = true;
+        }
+        if(!flag)
+            return -1;
+        return n;
+    }
+
+
+    public int[] supplyWagon(int[] supplies) {
+        int n = supplies.length;
+        List<Integer> list = new ArrayList<>();
+        for(int m:supplies)
+            list.add(m);
+        while (true){
+            int min = list.get(0)+list.get(1);
+            int pos = 0;
+            for(int i=1;i<list.size()-1;i++){
+                int val = list.get(i)+list.get(i+1);
+                if(val<min){
+                    min = val;
+                    pos = i;
                 }
             }
+            int sum = list.get(pos)+list.get(pos+1);
+            list.remove(pos);
+            list.remove(pos);
+            list.add(pos,sum);
+            if(list.size()<=n/2)
+                break;
         }
-        List<int[]> list = new ArrayList<>();
-        while (!stack.isEmpty()){
-            list.add(stack.pop());
-        }
-        Collections.reverse(list);
-        int pos = 0;
-        for(int i=0;i<s.length();i++){
-            if(pos<list.size()&&i==list.get(pos)[1]){
-                pos++;
-                continue;
-            }
-            char ch = s.charAt(i);
-            sb.append(ch);
-        }
-        return sb.toString();
+        int[] ans = new int[n/2];
+        for(int i=0;i<list.size();i++)
+            ans[i] = list.get(i);
+        return ans;
+
     }
 
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{8,11,17,2,25,29,21,20,4,22};
-        int[][] nums1 = new int[][]{{1,1},{500000000,499999999},{1000000000,999999998}};
-        int[][] nums2 = new int[][]{{1,3},{0,0},{0,3},{4,2},{1,0}};
+        int[] nums = new int[]{410193,229980,600441};
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++)
+                System.out.println(gcd(nums[i],nums[j]));
+        }
+
     }
 }
