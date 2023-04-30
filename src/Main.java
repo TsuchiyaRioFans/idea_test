5c5dc34f9225bb98f539a938d1a2686958b92a18
zhz <zhz15114585761@hrbeu.edu.cn>
2023-04-30 09:54:54 +0800

    2023-04-30

--- src/Main.java
+++ src/Main.java
@@ -43,41 +43,6 @@ public class Main {
       }
     }
 
-    public List<List<Integer>> findMatrix(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        boolean[] visited = new boolean[n];
-        Arrays.sort(nums);
-        int count = 0;
-        while (true){
-            Set<Integer> set = new HashSet<>();
-            for(int i=0;i<n;i++){
-                if(visited[i])
-                    continue;
-                if(set.contains(nums[i]))
-                    continue;
-                set.add(nums[i]);
-                visited[i] = true;
-            }
-            count+=set.size();
-            ans.add(new ArrayList<>(set));
-            if(count==n)
-                break;
-        }
-        return ans;
-    }
-
-    public long[] findPrefixScore(int[] nums) {
-        int n = nums.length;
-        long[] ans = new long[n];
-        int max = nums[0];
-        ans[0] = 2L *nums[0];
-        for(int i=1;i<n;i++){
-            max = Math.max(max,nums[i]);
-            ans[i] = nums[i]+max;
-        }
-        return ans;
-    }
 
     public int lastStoneWeightII(int[] ss) {
         int n = ss.length;
@@ -97,136 +62,107 @@ public class Main {
     }
 
 
-    public String solveEquation(String s) {
-        int x = 0,num = 0;
-        int n = s.length();
-        for(int i=0,op=1;i<n;){
-            char ch = s.charAt(i);
-            if(ch=='+'){
-                op=1;i++;
+    public int longestStrChain(String[] words) {
+        Map<String,Integer> map = new HashMap<>();
+        Arrays.sort(words,(a,b)->a.length()-b.length());
+        int ans = 0;
+        for(String s:words){
+            map.put(s,1);
+            for(int i=0;i<s.length();i++){
+                String prev = s.substring(0,i)+s.substring(i+1);
+                if(map.containsKey(prev)){
+                    int max = Math.max(map.get(s),1+map.get(prev));
+                    map.put(s,max);
+                }
             }
-            else if(ch=='-'){
-                op=-1;i++;
+            ans = Math.max(ans,map.get(s));
+        }
+        return ans;
+    }
+
+    public int minOperations(int[] nums) {
+        int n = nums.length, gcdAll = 0, cnt1 = 0;
+        for (int x : nums) {
+            gcdAll = gcd(gcdAll, x);
+            if (x == 1)
+                ++cnt1;
+        }
+        if (gcdAll > 1)
+            return -1;
+        if (cnt1 > 0)
+            return n - cnt1;
+
+        int minSize = n;
+        for (int i = 0; i < n; ++i) {
+            int g = 0;
+            for (int j = i; j < n; ++j) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    // 这里本来是 j-i+1，把 +1 提出来合并到 return 中
+                    minSize = Math.min(minSize, j - i);
+                    break;
+                }
             }
-            else if(ch=='='){
-                op=1;x*=-1;num*=-1;i++;
+        }
+        return minSize + n - 1;
+    }
+
+    private int gcd(int a, int b) {
+        while (a != 0) {
+            int tmp = a;
+            a = b % a;
+            b = tmp;
+        }
+        return b;
+    }
+
+    public int fun1(int[] nums){
+        int ans = 0;
+        while (true){
+            boolean flag = true;
+            for(int i=1;i<3;i++){
+                if(nums[i]-nums[i-1]!=1){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                break;
+            int dis1 = nums[1]-nums[0];
+            int dis2 = nums[2]-nums[1];
+            ans++;
+            if(dis1==1||dis2==1){
+                break;
+            }
+            if(dis1>=dis2){
+                nums[0]=nums[1];
+                nums[1] = nums[0]+1;
             }
             else{
-                int j =i;
-                while (j<n){
-                    char ch1 = s.charAt(j);
-                    if(ch1=='+'||ch1=='-'||ch1=='=')
-                        break;
-                    j++;
-                }
-                if(s.charAt(j-1)=='x'){
-                    x+= op*(i<j-1?(Integer.parseInt(s.substring(i,j-1))):1);
-                }
-                else{
-                    num+=op*Integer.parseInt(s.substring(i,j));
-                }
-                i=j;
-            }
-        }
-        if(x==0)
-            return num==0?"Infinite solutions":"No solution";
-        return "x="+(num/-x);
-    }
-
-    public int sumOfMultiples(int n) {
-        int ans = 0;
-        for(int i=1;i<=n;i++){
-            if(i%3==0||i%5==0||i%7==0){
-                ans+=i;
+                nums[2] = nums[1];
+                nums[1] = nums[2]-1;
             }
         }
         return ans;
     }
 
-    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
-        int n = nums.length;
-        int[] ans = new int[n-k+1];
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<k;i++)
-            list.add(nums[i]);
-        Collections.sort(list);
-        int left = 0;
-        int right = k;
-        for(int i=0;i<ans.length;i++){
-            int val = list.get(x-1);
-            if(val<0)
-                ans[i] = val;
-            if(right<n){
-                int pos1 = Collections.binarySearch(list,nums[left]);
-                list.remove(pos1);
-                int pos2 = Collections.binarySearch(list,nums[right]);
-                if(pos2<0)
-                    pos2 = -1*pos2 -1;
-                list.add(pos2,nums[right]);
-                left++;
-                right++;
-            }
-        }
+    public int fun2(int[] nums){
+        int ans = nums[1]-nums[0]+nums[2]-nums[1]-2;
         return ans;
     }
-
-    public static int gcd(int a, int b) {
-        return b != 0 ? gcd(b, a%b) : a;
-    }
-    public int minOperations(int[] nums) {
-        boolean flag = false;
-        int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            int val1 = nums[i];
-            int val2 = nums[i+1];
-            if(val1%2==0&&val2%2==1)
-                flag = true;
-            if(val1%2==1&&val2%2==0)
-                flag = true;
-        }
-        if(!flag)
-            return -1;
-        return n;
-    }
-
-
-    public int[] supplyWagon(int[] supplies) {
-        int n = supplies.length;
-        List<Integer> list = new ArrayList<>();
-        for(int m:supplies)
-            list.add(m);
-        while (true){
-            int min = list.get(0)+list.get(1);
-            int pos = 0;
-            for(int i=1;i<list.size()-1;i++){
-                int val = list.get(i)+list.get(i+1);
-                if(val<min){
-                    min = val;
-                    pos = i;
-                }
-            }
-            int sum = list.get(pos)+list.get(pos+1);
-            list.remove(pos);
-            list.remove(pos);
-            list.add(pos,sum);
-            if(list.size()<=n/2)
-                break;
-        }
-        int[] ans = new int[n/2];
-        for(int i=0;i<list.size();i++)
-            ans[i] = list.get(i);
+    public int[] numMovesStones(int a, int b, int c) {
+        int[] ans = new int[2];
+        int[] temp1 = new int[]{a,b,c};
+        Arrays.sort(temp1);
+        int[] temp2 = Arrays.copyOf(temp1,3);
+        ans[0] = fun1(temp1);
+        ans[1] = fun2(temp2);
         return ans;
-
     }
-
     public static void main(String[] args) {
         Main main = new Main();
 //        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{410193,229980,600441};
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++)
-                System.out.println(gcd(nums[i],nums[j]));
-        }
-
+        int[] nums = new int[]{0,0};
+        main.numMovesStones(1,3,2);
     }
 }
