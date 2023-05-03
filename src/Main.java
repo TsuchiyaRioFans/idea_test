adb7842bcabeb29a17d97bae355a92c42c1dcd0a
zhz <zhz15114585761@hrbeu.edu.cn>
2023-05-03 20:02:31 +0800

    2023.05.03

--- src/Main.java
+++ src/Main.java
@@ -61,108 +61,64 @@ public class Main {
         return Math.abs(sum - f[n][t] - f[n][t]);
     }
 
-
-    public int longestStrChain(String[] words) {
-        Map<String,Integer> map = new HashMap<>();
-        Arrays.sort(words,(a,b)->a.length()-b.length());
-        int ans = 0;
-        for(String s:words){
-            map.put(s,1);
-            for(int i=0;i<s.length();i++){
-                String prev = s.substring(0,i)+s.substring(i+1);
-                if(map.containsKey(prev)){
-                    int max = Math.max(map.get(s),1+map.get(prev));
-                    map.put(s,max);
+    public int videoStitching(int[][] clips, int time) {
+        int[] dp = new int[time + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int i = 1; i <= time; i++) {
+            for (int[] clip : clips) {
+                if (clip[0] < i && i <= clip[1]) {
+                    dp[i] = Math.min(dp[i], dp[clip[0]] + 1);
                 }
             }
-            ans = Math.max(ans,map.get(s));
         }
-        return ans;
+        return dp[time] == Integer.MAX_VALUE - 1 ? -1 : dp[time];
     }
 
-    public int minOperations(int[] nums) {
-        int n = nums.length, gcdAll = 0, cnt1 = 0;
-        for (int x : nums) {
-            gcdAll = gcd(gcdAll, x);
-            if (x == 1)
-                ++cnt1;
-        }
-        if (gcdAll > 1)
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        if((long)m*(long)k>n)
             return -1;
-        if (cnt1 > 0)
-            return n - cnt1;
-
-        int minSize = n;
-        for (int i = 0; i < n; ++i) {
-            int g = 0;
-            for (int j = i; j < n; ++j) {
-                g = gcd(g, nums[j]);
-                if (g == 1) {
-                    // 这里本来是 j-i+1，把 +1 提出来合并到 return 中
-                    minSize = Math.min(minSize, j - i);
-                    break;
+        int left = 0;
+        int right = Arrays.stream(bloomDay).max().getAsInt();
+        int mid;
+        while (left<right){
+            mid = left+(right-left)/2;
+            boolean[] isVisited = new boolean[n];
+            for(int i=0;i<n;i++){
+                if(bloomDay[i]<=mid)
+                    isVisited[i] = true;
+            }
+            int count = 0;
+            int count1 = 0;
+            int pos = 0;
+            while (pos<n){
+                if(isVisited[pos]){
+                    count1++;
+                    if(count1==k){
+                        count++;
+                        count1=0;
+                    }
                 }
-            }
-        }
-        return minSize + n - 1;
-    }
-
-    private int gcd(int a, int b) {
-        while (a != 0) {
-            int tmp = a;
-            a = b % a;
-            b = tmp;
-        }
-        return b;
-    }
-
-    public int fun1(int[] nums){
-        int ans = 0;
-        while (true){
-            boolean flag = true;
-            for(int i=1;i<3;i++){
-                if(nums[i]-nums[i-1]!=1){
-                    flag = false;
-                    break;
+                else{
+                    count1 = 0;
                 }
+                pos++;
             }
-            if(flag)
-                break;
-            int dis1 = nums[1]-nums[0];
-            int dis2 = nums[2]-nums[1];
-            ans++;
-            if(dis1==1||dis2==1){
-                break;
-            }
-            if(dis1>=dis2){
-                nums[0]=nums[1];
-                nums[1] = nums[0]+1;
+            if(count>=m){
+                right = mid;
             }
             else{
-                nums[2] = nums[1];
-                nums[1] = nums[2]-1;
+                left = mid+1;
             }
         }
-        return ans;
+        return left;
     }
 
-    public int fun2(int[] nums){
-        int ans = nums[1]-nums[0]+nums[2]-nums[1]-2;
-        return ans;
-    }
-    public int[] numMovesStones(int a, int b, int c) {
-        int[] ans = new int[2];
-        int[] temp1 = new int[]{a,b,c};
-        Arrays.sort(temp1);
-        int[] temp2 = Arrays.copyOf(temp1,3);
-        ans[0] = fun1(temp1);
-        ans[1] = fun2(temp2);
-        return ans;
-    }
+
     public static void main(String[] args) {
         Main main = new Main();
-//        main.getNums("[[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]");
-        int[] nums = new int[]{0,0};
-        main.numMovesStones(1,3,2);
+        int[] nums = new int[]{0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10};
+        int[][] nums1 = new int[][]{{0,1},{1,2}};
     }
 }
