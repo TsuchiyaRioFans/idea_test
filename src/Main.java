149aceaac7c7d52500cd32432e94261ec6df943f
zhz <zhz15114585761@hebeu.edu.cn>
2023-05-09 10:29:14 +0800

    test2

--- src/Main.java
+++ src/Main.java
@@ -179,5 +179,6 @@ public class Main {
         Main main = new Main();
         int[] nums = new int[]{0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10};
         int[][] nums1 = new int[][]{{0,1},{1,2}};
+        System.out.println(1);
     }
 }
