05db189e625455f75c26eacd6da5108a0fa1de55
zhz <zhz15114585761@hrbeu.edu.cn>
2023-05-13 08:46:57 +0800

    2023-05-13

--- src/Main.java
+++ src/Main.java
@@ -115,6 +115,28 @@ public class Main {
         return left;
     }
 
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> radiant = new LinkedList<Integer>();
+        Queue<Integer> dire = new LinkedList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (senate.charAt(i) == 'R') {
+                radiant.offer(i);
+            } else {
+                dire.offer(i);
+            }
+        }
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int radiantIndex = radiant.poll(), direIndex = dire.poll();
+            if (radiantIndex < direIndex) {
+                radiant.offer(radiantIndex + n);
+            } else {
+                dire.offer(direIndex + n);
+            }
+        }
+        return !radiant.isEmpty() ? "Radiant" : "Dire";
+    }
+
 
     public static void main(String[] args) {
         Main main = new Main();
